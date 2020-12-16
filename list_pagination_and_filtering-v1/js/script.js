166ed872bcb919cc5d6b7635006b8d97d36bbf9e
Adrial95 <64555906+Adrial95@users.noreply.github.com>
2020-12-15 18:50:00 -0800

    12/15/2020

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -22,67 +22,57 @@ FSJS project 2 - List Filter and Pagination
 function showPage (list, page) {
    var startIndex = (page * 9) - 9;
    var endIndex = (page *9 );
-   const studentList = documetn.querySelector(".student-list");
+   const studentList = document.querySelector(".student-list");
    studentList.innerHTML = "";
 
-   for(let i = 0; i < list.Lenght; i=+1) {
+   for(let i = 0; i < list.Lenght; i += 1) {
     if (i >= startIndex && i <= endIndex ) {
      let studentItem =   
-     `div class="pagination">
-     <ul>
-       <li>
-         <a class="active" href="#">1</a>
-       </li>
-        <li>
-         <a href="#">2</a>
-       </li>
-        <li>
-         <a href="#">3</a>
-       </li>
-        <li>
-         <a href="#">4</a>
-       </li>
-        <li>
-         <a href="#">5</a>
-       </li>
-     </ul>
-   </div>`;
+     `<li class="student-item cf">
+     <div class="student-details">
+        <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
+        <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
+        <span class="email">${list[i].email}</span>
+     </div>
+     <div class="joined-details">
+        <span class="date">Joined ${list[i].registered.date}</span>
+      </div>
+     </li>`;
      studentList.insertAdjecentHTML("beforeend", studentItem);
     }
    }
    return list;
-}
+};
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-function appendPageLinks(list) {
-   let numOfPages = math.ceil(list.lenght/9);
+function addPagination(list) {
+   let numOfPages = Math.ceil(list.lenght/9);
    const linkList = document.querySelector(".link-list");
    linkList.innerHTML = "";
 
-   for(var i = 1; i <=numOfPages; i =+ 1) {
+   for(var i = 1; i <= numOfPages; i =+ 1) {
    let button =
    `<li>
    <button type="button">${[i]}</button>
-</li>`;
-linkList.insertAdjacentHTML("beforeend",  button);
-const firstButton = document.querySelector("button");
-firstButton.classname("active"); 
-
-linkList.addEventListener('click', (e) =>{
- if (e.target.tagName === "BUTTON") {
-    var activeButton = document.querySelector(".active");
+   </li>`;
+   linkList.insertAdjacentHTML("beforeend",  button);
+   const firstButton = document.querySelector("button");
+   firstButton.classname("active"); 
+   }
+   linkList.addEventListener("click", (e) => {
+   if (e.target.tagName === "BUTTON") {
+    const activeButton = document.querySelector(".active");
     activeButton.className = "";
     const inactiveButton = e.target;
     inactiveButton.className  = "active";
     showPage(data, e.target.texContent);
 
    }
-  })
- }
-}
+  });
+};
 
 showPage(data, 1);
 addPagination(data);
